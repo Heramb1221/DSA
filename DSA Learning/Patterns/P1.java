116152f0c1484eae11f67abdc960101167908bbd
Heramb Chaudhari <169079872+Heramb1221@users.noreply.github.com>
2025-01-07 10:28:22 +0530

    Pattern Problems

---
+++ DSA Learning/Patterns/P1.java
@@ -0,0 +1,14 @@
+public class P1 {
+    public static void main(String[] args) {
+        pattern(4);
+    }
+
+    static void pattern(int n) {
+        for(int row = 1; row <=n; row++) {
+            for(int col = 1; col <=n; col++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ DSA Learning/Patterns/P3.java
@@ -0,0 +1,14 @@
+public class P3 {
+    public static void main(String[] args) {
+        pattern(5);
+    }
+
+    static void pattern(int n) {
+        for(int row = 1; row <= n; row++) {
+            for(int col = 1; col <= n-row+1; col++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
