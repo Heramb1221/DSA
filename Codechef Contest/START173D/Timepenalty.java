8134abc05a0970bff5863521c032bed7c574e2c1
Heramb Chaudhari <169079872+Heramb1221@users.noreply.github.com>
2025-02-12 20:53:49 +0530

    12-02-25

---
+++ Codechef Contest/START173D/Timepenalty.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Timepenalty
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        int r = x + (10*y);
+        System.out.println(r);
+	}
+}
