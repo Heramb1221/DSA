df7935f4ac5737e9a1db183fe4ebb169d8c94266
Heramb Chaudhari <169079872+Heramb1221@users.noreply.github.com>
2025-10-30 09:41:15 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (53.50%) - LeetSync

---
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
@@ -0,0 +1,50 @@
+class Solution {
+    public boolean check(int[] nums) {
+
+        int c = findPivot(nums) + 1;
+
+        if(c == -1) {
+            return false;
+        }
+
+        for(int i = 0; i < nums.length - 1; i++) {
+            if(nums[(i+c) % nums.length] > nums[(i+c+1)%nums.length]) {
+                return false;
+            }
+        }
+
+        return true;
+        
+    }
+
+    int findPivot(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (mid < end && arr[mid] > arr[mid + 1]) {
+                return mid;
+            }
+            if (mid > start && arr[mid] < arr[mid - 1]) {
+                return mid-1;
+            }
+            if (arr[mid] == arr[start] && arr[mid] == arr[end]) {
+                if (start < end && arr[start] > arr[start + 1]) {
+                    return start;
+                }
+                start++;
+
+                if (end > start && arr[end] < arr[end - 1]) {
+                    return end - 1;
+                }
+                end--;
+            }
+            else if(arr[start] < arr[mid] || (arr[start] == arr[mid] && arr[mid] > arr[end])) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
